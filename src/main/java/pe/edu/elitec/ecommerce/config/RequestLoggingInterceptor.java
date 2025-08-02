560236722c01778dced9fb8043606d649c96dc00
Jhon Loyola <jhon.loyola@elitec.edu.pe>
2025-08-01 20:33:10 -0500

    Update del Request Interceptor

--- src/main/java/pe/edu/elitec/ecommerce/config/RequestLoggingInterceptor.java
+++ src/main/java/pe/edu/elitec/ecommerce/config/RequestLoggingInterceptor.java
@@ -7,13 +7,20 @@ import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.View;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class RequestLoggingInterceptor implements ClientHttpRequestInterceptor {
 
     private static final Logger logger = LoggerFactory.getLogger(RequestLoggingInterceptor.class);
+    private final View error;
+
+    public RequestLoggingInterceptor(View error) {
+        this.error = error;
+    }
 
     @Override
     public ClientHttpResponse intercept(
@@ -23,31 +30,93 @@ public class RequestLoggingInterceptor implements ClientHttpRequestInterceptor {
 
         long startTime = System.currentTimeMillis();
 
-        logger.debug("🚀 External API Request: {} {}", request.getMethod(), request.getURI());
+        logDetailedRequest(request, body);
+
+        //logger.debug("🚀 External API Request: {} {}", request.getMethod(), request.getURI());
 
         try {
             ClientHttpResponse response = execution.execute(request, body);
 
             long duration = System.currentTimeMillis() - startTime;
 
-            logger.info("✅ External API Response: {} {} - Status: {} - Duration: {}ms",
+            /*logger.info("✅ External API Response: {} {} - Status: {} - Duration: {}ms",
                     request.getMethod(),
                     request.getURI(),
                     response.getStatusCode().value(),
-                    duration);
+                    duration);*/
+            logDetailedResponse(request, response, duration);
 
             return response;
 
         } catch (IOException e) {
             long duration = System.currentTimeMillis() - startTime;
 
-            logger.error("🔥 External API Error: {} {} - Duration: {}ms - Error: {}",
+            /*logger.error("🔥 External API Error: {} {} - Duration: {}ms - Error: {}",
                     request.getMethod(),
                     request.getURI(),
                     duration,
-                    e.getMessage());
+                    e.getMessage());*/
+            logDetailedError(request, e, duration);
 
             throw e;
         }
     }
+
+
+
+    private void logDetailedRequest(HttpRequest request, byte[] body) {
+        logger.info("=== EXTERNAL API REQUEST ===");
+        logger.info("Method & URL: {} {}", request.getMethod(), request.getURI());
+        logger.info("Headers: {}", request.getHeaders());
+
+        if(body.length > 0) {
+            String bodyString = new String(body, StandardCharsets.UTF_8);
+            logger.info("Request Body: {}", bodyString);
+        }
+
+        String correlationId = request.getHeaders().getFirst("X-Correlation-ID");
+        if (correlationId!=null){
+            logger.info("Correlational-ID: {}", correlationId);
+        }
+
+        logger.info("Request sent at: {}", System.currentTimeMillis());
+
+    }
+
+
+    private void logDetailedResponse(HttpRequest request, ClientHttpResponse response, long duration) throws IOException {
+        logger.info("=== EXTERNAL API RESPONSE ===");
+        logger.info("URL: {} {}", request.getMethod(), request.getURI());
+        logger.info("Status Code: {} {}", response.getStatusCode().value(),response.getStatusText());
+        logger.info("Response Headers: {}", response.getHeaders());
+        logger.info("Duration: {}ms", duration);
+
+        if(duration < 1000){
+            logger.info("Performance: FAST (< 1s)");
+        } else if (duration < 3000) {
+            logger.warn("Performance: SLOW (1 -3s)");
+        } else {
+            logger.error("Performance: VERY SLOW (> 3s)");
+        }
+
+        logger.info("===============================");
+    }
+
+    private void logDetailedError(HttpRequest request, IOException error, long duration) {
+        logger.error("=== EXTERNAL API ERROR ===");
+        logger.error("URL: {} {}", request.getMethod(), request.getURI());
+        logger.error("Duration before error: {}ms", duration);
+        logger.error("Error Type: {}", error.getClass().getSimpleName());
+        logger.error("Error Message: {}", error.getMessage());
+
+        if(error.getMessage().contains("timeout")){
+            logger.error("DIAGNOSIS: Timeout error - EXTERNAL API is slow");
+        }else if (error.getMessage().contains("connection")){
+            logger.error("DIAGNOSIS: Connection error - Network or DNS issue");
+        }else {
+            logger.error("DIAGNOSIS: Unknown error - Check error details above");
+        }
+        logger.info("===============================");
+    }
+
 }
